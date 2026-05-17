b5341eac2a36e8a5f81aaa4291623a62b3ff2bac
Naksito <alex.bin.phan@gmail.com>
2026-05-17 19:11:13 +0300

    fix frontend

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,3 @@
 export const environment = {
-  apiGateway: 'http://100.80.222.26:31000/api', // OptiPlex Server deployment
-  // apiGateway: 'http://localhost:8080/api', // Local testing
+  apiGateway: '/api', // Proxied through Nginx (production)
 };
